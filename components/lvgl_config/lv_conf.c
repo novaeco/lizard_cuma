9feff834e2a67fd082eb269e38200cd856042522
novaeco <novatribolnovea@gmail.com>
2025-07-18 09:00:27 +0200

    Delete components directory

--- components/lvgl_config/lv_conf.c
+++
@@ -1,9 +0,0 @@
-/**
- * @file lv_conf.c
- * @brief Configuration LVGL pour ESP32-S3
- */
-
-#include "lv_conf.h"
-
-// Ce fichier peut être vide pour l'instant
-// Il sera utilisé pour des configurations spécifiques si nécessaire
